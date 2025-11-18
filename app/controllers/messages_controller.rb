1701765ff8c62f32d1f922f98d790782c584cbe4
fukuhara3312 <233537861+fukuhara3312@users.noreply.github.com>
2025-11-18 10:43:18 +0900

    [update]DM画面実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.find(params[:room_id])
+
+    return redirect_back(fallback_location: root_path) unless
+      @room.entries.exists?(user_id: current_user.id)
+
+    @message = @room.messages.new(message_params)
+    @message.user_id = current_user.id
+
+    if @message.save
+      redirect_to room_path(@room)
+    else
+      @messages = @room.messages.order(created_at: :asc)
+      render "rooms/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message)
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,35 +1,63 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
 
+  # -----------------------------
+  # ルーム一覧（相手の一覧）
+  # -----------------------------
+  def index
+    # 自分が参加しているroom_id一覧
+    my_room_ids = current_user.entries.pluck(:room_id)
+
+    # 相手のentry（＝自分以外のユーザーが入っているroom）
+    @another_entries = Entry
+                         .where(room_id: my_room_ids)
+                         .where.not(user_id: current_user.id)
+                         .preload(room: :messages)
+                         .preload(user: { icon_attachment: :blob })
+  end
+
+  # -----------------------------
+  # ルーム作成（初回DM開始）
+  # -----------------------------
   def create
     @room = Room.create
-    @current_entry = @room.entries.create(user_id: current_user.id)
+
+    # 自分
+    @room.entries.create(user_id: current_user.id)
+
+    # 相手
     another_user_id = params.dig(:entry, :user_id)
-    if another_user_id
-        @another_entry = @room.entries.create(user_id: another_user_id)
-    end
+    @room.entries.create(user_id: another_user_id)
+
     redirect_to room_path(@room)
   end
 
-  def index
-    my_room_id = current_user.entries.pluck(:room_id)
-    @another_entries = Entry
-                       .where(room_id: my_room_id)
-                       .where.not(user_id: current_user.id)
-                       .preload(room: :messages).preload(user: { icon_attachment: :blob })
-  end
-
+  # -----------------------------
+  # ルーム表示（メッセージ一覧）
+  # -----------------------------
   def show
     @room = Room.find(params[:id])
-    if @room.entries.where(user_id: current_user.id).present?
-      @messages = @room.messages.all
-      @message = Message.new
-      @entries = @room.entries
-      # 相手ユーザーの Entry を取得
-      @another_entry = @entries.where.not(user_id: current_user.id).first
-      @another_entry ||= OpenStruct.new(user: OpenStruct.new(name: "UserName"))
-    else
-      redirect_back(fallback_location: root_path)
+
+    # 自分がこのroomに参加しているか確認
+    unless @room.entries.exists?(user_id: current_user.id)
+      return redirect_back(fallback_location: root_path)
     end
+
+    @entries = @room.entries.includes(:user)
+
+    # 相手ユーザー
+    @another_user = @entries.where.not(user_id: current_user.id).first&.user
+
+    # メッセージ一覧
+    @messages = @room.messages.order(created_at: :asc)
+
+    # 新規メッセージフォーム用
+    @message = Message.new
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
